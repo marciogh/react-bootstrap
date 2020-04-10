60e5eba4c9541e399e9ec530f49b93170c533885
Marcio Ghiraldelli <mghiraldelli@atlassian.com>
2020-04-10 10:42:21 +1000

    initial import

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: "html-loader"
+                  }
+                ]
+            },            
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                  loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ]    
+};
